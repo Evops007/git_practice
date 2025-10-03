48adaafce5eb3a4a3b35d8d418d8a22b7c71a5b2
EvOps007 <even.kr@hotmail.com>
2025-10-03 13:17:02 +0200

    Random sentence function is done

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Random message generator
 
-**This is my first portfoil project at CodeCademy.
+**This is my first portfoil project at CodeCademy.**
 This program generates a random message, like: "You should go for a walk"
--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-//This is a test
+const subjectVerb = ["you are", "you should", "you can"];
+const verb = ["go for", "trust", "eat", "learn"];
+const goForObject = ["a walk", "it", "the opportunity"];
+const trustObject = ["your gut feeling", "yourself", "your family"]
+const eatObject = ["some cake", "healthy food", "a snack"];
+const learnObject = ["a new language", "to code", "to play guitar"];
+const youAreObject = ["a great", "an amazing", "the best"];
+const youAreNouns = ["friend", "person", "developer", "baker"];
+const punctuation = [".", "!", "!!"];
+
+const randomSentenceGenerator = () => {
+    let randomSentence = [];
+    randomSentence.push(subjectVerb[Math.floor(Math.random() * subjectVerb.length)]);
+    
+    if (randomSentence[0] === "you are") {
+        randomSentence.push(youAreObject[Math.floor(Math.random() * youAreObject.length)], youAreNouns[Math.floor(Math.random() * youAreNouns.length)]);
+    } else if (randomSentence[0] === "you should" || randomSentence[0] === "you can") {
+        randomSentence.push(verb[Math.floor(Math.random() * verb.length)]);
+         if (randomSentence[1] === "go for") {
+          randomSentence.push(goForObject[Math.floor(Math.random() * goForObject.length)] || randomSentence.push(trustObject[Math.floor(Math.random() * trustObject.length)]));
+         } else if (randomSentence[1] === "trust") {
+            randomSentence.push(trustObject[Math.floor(Math.random() * trustObject.length)]);
+         } else if (randomSentence[1] === "eat") {
+            randomSentence.push(eatObject[Math.floor(Math.random() * eatObject.length)]);
+         } else if (randomSentence[1] === "learn") {
+            randomSentence.push(learnObject[Math.floor(Math.random() * learnObject.length)]);
+         }
+    } 
+    
+    return `${randomSentence.join(" ")}${punctuation[Math.floor(Math.random() * punctuation.length)]}`;
+}
+
+console.log(randomSentenceGenerator());
